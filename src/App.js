9ca034e24f2c9ef23038740cf1f034412c75d42f
gaelzarco <gaelzarco@yahoo.com>
2022-02-09 16:21:14 -0800

    finished assignment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function App() {
+  let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter
+  return  (
+    <div>
+      <h1>{counter}</h1>
+      <h2>{counter}</h2>
+    </div>
+  )
+}
+
+export default App
